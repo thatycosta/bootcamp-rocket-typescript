d8612feaa17885d7361c03f3e4c4bb510df222dd
Tauane Costa <tauanedasilvacosta@gmail.com>
2021-01-24 16:11:45 -0300

    typescript first steps

---
+++ src/services/CreateUser.ts
@@ -0,0 +1,23 @@
+interface TechObject{
+    title: string;
+    experience: number;
+}
+
+interface CreateUserData{
+    name?: string;
+    email: string;
+    password: string;
+    techs: Array<string | TechObject>; //tipo unico string[]
+
+}
+
+
+export default function createUser({name, email, password}: CreateUserData){
+    const user = {
+        name,
+        email,
+        password,
+    }
+
+    return user;
+}
